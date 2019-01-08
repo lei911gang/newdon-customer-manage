083481a2327e4303c03cb60d2426deb103c99366
lei911gang <89172572@qq.com>
2019-01-08 22:37:37 +0800

    新增登录接口

--- src/main/java/com/newdon/NewdonApplication.java
+++ src/main/java/com/newdon/NewdonApplication.java
@@ -1,7 +1,6 @@
 package com.newdon;
 
 import com.github.wxiaoqi.merge.EnableAceMerge;
-import org.apache.commons.codec.binary.Base64;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,12 +13,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NewdonApplication {
     public static void main(String[] args) {
-//        SpringApplication.run(NewdonApplication.class, args);
-        String str = "zhangsan";
-        String s = Base64.encodeBase64String(str.getBytes());
-        System.out.println(s);
-        byte[] bytes = Base64.decodeBase64(s);
-        String s1 = new String(bytes);
-        System.out.println(s1);
+        SpringApplication.run(NewdonApplication.class, args);
     }
 }
--- src/main/java/com/newdon/controller/UserController.java
+++ src/main/java/com/newdon/controller/UserController.java
@@ -3,7 +3,6 @@ package com.newdon.controller;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.newdon.base.Result;
 import com.newdon.entity.User;
-import com.newdon.service.TechnologyInfoService;
 import com.newdon.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import sun.misc.BASE64Decoder;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -31,18 +29,19 @@ public class UserController {
 
     @PostMapping(value = "/login")
     public Result login(@RequestParam("username") String username, @RequestParam("password") String password, HttpServletRequest request) {
-//        Base64.decodeBase64();
-//        Base64.encodeBase64();
-        //TODO
+        byte[] usernameBytes = Base64.decodeBase64(username);
+        byte[] passwordBytes = Base64.decodeBase64(password);
+        username = new String(usernameBytes);
+        password = new String(passwordBytes);
         EntityWrapper<User> wrapper = new EntityWrapper();
         wrapper.eq("username", username);
         wrapper.eq("password", DigestUtils.md5Hex(password));
         User user = this.userService.selectOne(wrapper);
         if (null != user) {
             request.getSession().setAttribute("username", username);
+            return Result.build(200, "OK", username);
         } else {
             return Result.build(500, "FAILED", "用户名或密码错误!");
         }
-        return Result.build(200, "OK", username);
     }
 }
