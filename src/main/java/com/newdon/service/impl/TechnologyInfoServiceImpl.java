fa3ac34069bcb972c56469963b012e9a55296434
leigang@flashhold.com <leigang@flashhold.com>
2019-02-11 18:09:54 +0800

    修复聚合查询bug

--- src/main/java/com/newdon/service/impl/TechnologyInfoServiceImpl.java
+++ src/main/java/com/newdon/service/impl/TechnologyInfoServiceImpl.java
@@ -31,20 +31,23 @@ public class TechnologyInfoServiceImpl extends ServiceImpl<TechnologyInfoMapper,
     public Map<String, String> getSystemLevelAndQuantity(String contractId) {
         Map<String, String> map = new HashMap<>();
         //TODO 查询出该合同ID所有的系统级别以及系统数量，合并为字符串
-        SystemLevelAndQuantity s = new SystemLevelAndQuantity();
-        s.setContractId(contractId);
-        List<SystemLevelAndQuantity> list = this.systemLevelAndQuantityService.selectList(new EntityWrapper<>(s));
-        if (null != list && list.size() > 0) {
-            StringBuilder str = new StringBuilder();
-            //合并为字符串
-            for (SystemLevelAndQuantity sl : list) {
-                String systemLevel = sl.getSystemLevel();
-                Integer systemQuantity = sl.getSystemQuantity();
-                str.append(systemLevel).append("=").append(systemQuantity).append("\n");
+        String[] split = contractId.split(",");
+        for (int i = 0; i < split.length; i++) {
+            SystemLevelAndQuantity s = new SystemLevelAndQuantity();
+            s.setContractId(split[i]);
+            List<SystemLevelAndQuantity> list = this.systemLevelAndQuantityService.selectList(new EntityWrapper<>(s));
+            if (null != list && list.size() > 0) {
+                StringBuilder str = new StringBuilder();
+                //合并为字符串
+                for (SystemLevelAndQuantity sl : list) {
+                    String systemLevel = sl.getSystemLevel();
+                    Integer systemQuantity = sl.getSystemQuantity();
+                    str.append(systemLevel).append("=").append(systemQuantity).append("个").append(",");
+                }
+                map.put(split[i], str.toString());
+            } else {
+                map.put(split[i], "无");
             }
-            map.put(contractId, str.toString());
-        } else {
-            map.put(contractId, "无");
         }
         return map;
     }
@@ -53,20 +56,23 @@ public class TechnologyInfoServiceImpl extends ServiceImpl<TechnologyInfoMapper,
     public Map<String, String> getDeviceInformationAndQuantity(String contractId) {
         //TODO 查询出该合同ID所有的设备信息和数量，合并为字符串
         Map<String, String> map = new HashMap<>();
-        DeviceInformationAndQuantity d = new DeviceInformationAndQuantity();
-        d.setContractId(contractId);
-        List<DeviceInformationAndQuantity> list = this.deviceInformationAndQuantityService.selectList(new EntityWrapper<>(d));
-        if (null != list && list.size() > 0) {
-            StringBuilder str = new StringBuilder();
-            //合并为字符串
-            for (DeviceInformationAndQuantity di : list) {
-                String deviceInfo = di.getDeviceInfo();
-                Integer deviceQuantity = di.getDeviceQuantity();
-                str.append(deviceInfo).append("=").append(deviceQuantity).append("\n");
+        String[] split = contractId.split(",");
+        for (int i = 0; i < split.length; i++) {
+            DeviceInformationAndQuantity d = new DeviceInformationAndQuantity();
+            d.setContractId(split[i]);
+            List<DeviceInformationAndQuantity> list = this.deviceInformationAndQuantityService.selectList(new EntityWrapper<>(d));
+            if (null != list && list.size() > 0) {
+                StringBuilder str = new StringBuilder();
+                //合并为字符串
+                for (DeviceInformationAndQuantity di : list) {
+                    String deviceInfo = di.getDeviceInfo();
+                    Integer deviceQuantity = di.getDeviceQuantity();
+                    str.append(deviceInfo).append("=").append(deviceQuantity).append("个").append(",");
+                }
+                map.put(split[i], str.toString());
+            } else {
+                map.put(split[i], "无");
             }
-            map.put(contractId, str.toString());
-        } else {
-            map.put(contractId, "无");
         }
         return map;
     }
