ecf2724bf3e68c5d27d73b0fddcc126fb899ad34
leigang@flashhold.com <leigang@flashhold.com>
2019-03-01 15:24:13 +0800

    fix

--- src/main/java/com/newdon/controller/ClienteleInfoController.java
+++ src/main/java/com/newdon/controller/ClienteleInfoController.java
@@ -6,7 +6,9 @@ import com.newdon.base.Insert;
 import com.newdon.base.NewDonResult;
 import com.newdon.base.Update;
 import com.newdon.entity.ClienteleInfo;
+import com.newdon.entity.ContractInfo;
 import com.newdon.service.ClienteleInfoService;
+import com.newdon.service.ContractInfoService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +30,8 @@ import org.springframework.web.bind.annotation.*;
 public class ClienteleInfoController {
     @Autowired
     private ClienteleInfoService clienteleInfoService;
+    @Autowired
+    private ContractInfoService contractInfoService;
 
     @PostMapping(value = "/query")
     public NewDonResult query(ClienteleInfo clienteleInfo, Integer page, Integer rows) {
@@ -78,7 +82,7 @@ public class ClienteleInfoController {
         wrapper.eq("clientele_name", clienteleInfo.getClienteleName());
         int i = this.clienteleInfoService.selectCount(wrapper);
         if (i > 0) {
-            return NewDonResult.build(500, "FAILED", "客户已存在!");
+            return NewDonResult.build(500, "客户已存在!", "客户已存在!");
         }
         boolean insert = this.clienteleInfoService.insert(clienteleInfo);
         if (insert) {
@@ -113,6 +117,15 @@ public class ClienteleInfoController {
 
     @PostMapping(value = "/delete")
     public NewDonResult delete(@RequestParam("id") Long id) {
+        ClienteleInfo c = this.clienteleInfoService.selectById(id);
+        //有合同关联不允许删除
+        EntityWrapper<ContractInfo> wrapper = new EntityWrapper();
+        wrapper.eq("status", 1);
+        wrapper.like("clientele_name", c.getClienteleName());
+        int i = this.contractInfoService.selectCount(wrapper);
+        if (i > 0) {
+            return NewDonResult.build(500, "该客户有合同，无法删除", null);
+        }
         ClienteleInfo clienteleInfo = new ClienteleInfo();
         clienteleInfo.setId(id);
         clienteleInfo.setStatus(0);
