542d5a531df7bf7fd31f3b70d95902f655923a59
lei911gang <89172572@qq.com>
2019-05-27 21:30:20 +0800

    fix

--- src/main/java/com/newdon/interceptor/LoginInterceptor.java
+++ src/main/java/com/newdon/interceptor/LoginInterceptor.java
@@ -1,93 +1,92 @@
-//package com.newdon.interceptor;
-//
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.servlet.ModelAndView;
-//import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.util.HashSet;
-//import java.util.Set;
-//
-//@Component
-//public class LoginInterceptor extends HandlerInterceptorAdapter {
-//
-//    /**
-//     * 在请求被处理之前调用
-//     *
-//     * @param request
-//     * @param response
-//     * @param handler
-//     * @return
-//     * @throws Exception
-//     */
-//    @Override
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//        String basePath = request.getContextPath();
-//        String path = request.getRequestURI();
+package com.newdon.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+    /**
+     * 在请求被处理之前调用
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String basePath = request.getContextPath();
+        String path = request.getRequestURI();
 //        if (!doLoginInterceptor(path, basePath)) {//是否进行登陆拦截
 //            return true;
 //        }
-//        // 检查每个到来的请求对应的session域中是否有登录标识
-//        Object username = request.getSession().getAttribute("username");
-//        if (null == username || !(username instanceof String)) {
-//            // 未登录，重定向到登录页
-//            response.sendRedirect("/login");
-//            return false;
-//        }
-//        String userName = (String) username;
-//        return true;
-//    }
-//
-//    /**
-//     * 在请求被处理后，视图渲染之前调用
-//     *
-//     * @param request
-//     * @param response
-//     * @param handler
-//     * @param modelAndView
-//     * @throws Exception
-//     */
-//    @Override
-//    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-//        /*if (response.getStatus() == 500) {
-//            modelAndView.setViewName("/error/500");
-//        } else if (response.getStatus() == 404) {
-//            modelAndView.setViewName("/error/404");
-//        }*/
-//    }
-//
-//    /**
-//     * 在整个请求结束后调用
-//     *
-//     * @param request
-//     * @param response
-//     * @param handler
-//     * @param ex
-//     * @throws Exception
-//     */
-//    @Override
-//    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-//
-//    }
-//
-//    /**
-//     * 是否进行登陆过滤
-//     *
-//     * @param path
-//     * @param basePath
-//     * @return
-//     */
-//    private boolean doLoginInterceptor(String path, String basePath) {
-//        path = path.substring(basePath.length());
-//        Set<String> notLoginPaths = new HashSet<>();
-//        //设置不进行登录拦截的路径：登录注册和验证码
-//        //notLoginPaths.add("/");
-//        notLoginPaths.add("/login");
-//        notLoginPaths.add("/getin");
-//        if (notLoginPaths.contains(path)) {
-//            return false;
-//        }
-//        return true;
-//    }
-//}
+        // 检查每个到来的请求对应的session域中是否有登录标识
+        Object username = request.getSession().getAttribute("username");
+        if (null == username || !(username instanceof String)) {
+            // 未登录，重定向到登录页
+            response.sendRedirect("/newdon");
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 在请求被处理后，视图渲染之前调用
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        /*if (response.getStatus() == 500) {
+            modelAndView.setViewName("/error/500");
+        } else if (response.getStatus() == 404) {
+            modelAndView.setViewName("/error/404");
+        }*/
+    }
+
+    /**
+     * 在整个请求结束后调用
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @param ex
+     * @throws Exception
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+
+    /**
+     * 是否进行登陆过滤
+     *
+     * @param path
+     * @param basePath
+     * @return
+     */
+    private boolean doLoginInterceptor(String path, String basePath) {
+        path = path.substring(basePath.length());
+        Set<String> notLoginPaths = new HashSet<>();
+        //设置不进行登录拦截的路径：登录注册和验证码
+        //notLoginPaths.add("/");
+        notLoginPaths.add("/login");
+        notLoginPaths.add("/getin");
+        if (notLoginPaths.contains(path)) {
+            return false;
+        }
+        return true;
+    }
+}
